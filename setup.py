8fcfd012d96144de69e514bf79ebc2558d21d0fa
Shon Rockey <shonmon168@gmail.com>
2024-04-13 19:40:49 +0530

    Templete and setup.py added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'shon-Rocky',
+    author_email= 'shonmon168@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
